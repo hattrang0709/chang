3bedcebdae7493c2466591bd33422b6f8fa26de2
Thien Nguyen <ngkhanhthien94@gmail.com>
2021-02-02 21:40:52 +0700

    fix testng

--- src/main/java/Common/BaseTest.java
+++ src/main/java/Common/BaseTest.java
@@ -1,12 +1,17 @@
 package Common;
 
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
 public class BaseTest extends Driver{
 
-    public void openBrowser(){
+    @BeforeClass
+    public void openBrowser() {
         browser();
         driver.get(Constant.SEARCHPAGE.URL);
     }
 
+    @AfterClass
     public void closeBrowser(){
         driver.close();
     }
